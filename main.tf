add481726000d085d1e16eb5d93b560af78050ad
gadhiyamanthan009 <65277559+gadhiyamanthan009@users.noreply.github.com>
2022-12-26 18:56:59 +0530

    use model for security group

--- main.tf
+++ main.tf
@@ -24,19 +24,36 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
+  name  = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+  
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  cidr_blocks   = ["0.0.0.0/0"]
+
+  4gress_rules = ["all-all"]
+  cidr_blocks   = ["0.0.0.0/0"]
+
+}
+
+ 
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow HTTP and HTTPS and Allow Everything out"
   
   vpc_id = data.aws_vpc.default.id
 }
+
 
 resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
